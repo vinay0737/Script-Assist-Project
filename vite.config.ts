a53bf8109f0429b6984423cbbef1a8a3ffbc9b8c
vinay02103 <vekkulurivinay9390@gmail.com>
2025-02-12 16:06:06 +0530

    Updated vite config and removed old build files

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	server: {
-		port: 5175
-	},
-	plugins: [react()],
-	build: {
-		outDir: 'build'
-	}
+  server: {
+    port: 5175,
+  },
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
 });
